27a84b513b72c7a976b245d0e67db35d84a42bf2
Ayush20005 <ayushjaiswa.apsr405@gmail.com>
2026-06-27 18:43:41 +0530

    Time: 167 ms (58.32%) | Memory: 171.6 MB (48.14%) - LeetSync

--- 2270-find-all-lonely-numbers-in-the-array/find-all-lonely-numbers-in-the-array.cpp
+++ 2270-find-all-lonely-numbers-in-the-array/find-all-lonely-numbers-in-the-array.cpp
@@ -1,20 +1,25 @@
 class Solution {
 public:
     vector<int> findLonely(vector<int>& nums) {
-        int n=nums.size();
-        unordered_map<int,int>freq;
-        //count frequency of each element 
-        for(int i=0;i<n;i++){
-            freq[nums[i]]++;
-            //create hashmap 
-                    }
-        vector<int>ans;
-        //storing lonely pair 
-        for(int i=0;i<n;i++){
-            if(freq[nums[i]]==1 && freq[nums[i]+1]==0 && freq[nums[i]-1]==0){
-                ans.push_back(nums[i]);
+        unordered_map<int, int> freq;
+
+        // Store frequencies
+        for (int x : nums) {
+            freq[x]++;
+        }
+
+        vector<int> ans;
+
+        // Find lonely numbers
+        for (int x : nums) {
+            if (freq[x] == 1 &&
+                freq.count(x - 1) == 0 &&
+                freq.count(x + 1) == 0) {
+
+                ans.push_back(x);
             }
         }
+
         return ans;
     }
 };
