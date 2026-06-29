01063eb0e7eeea30b7bd7aec4ee945ba3698705b
Ayush20005 <ayushjaiswa.apsr405@gmail.com>
2026-06-29 18:52:29 +0530

    Time: 1 ms (54.91%) | Memory: 9.7 MB (75.95%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        int n=s.size();
+        int y=t.size();
+        if(n!=y){return false;}
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+       // if(s==t){
+            return s==t;
+       // }
+        return false;
+    }
+};
