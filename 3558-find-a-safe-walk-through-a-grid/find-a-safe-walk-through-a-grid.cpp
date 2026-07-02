c2c649fe1072a2961dcfa7769b82a26a4a8b0a81
Ayush20005 <ayushjaiswa.apsr405@gmail.com>
2026-07-02 15:50:05 +0530

    Time: 9 ms (86.13%) | Memory: 30.5 MB (84.59%) - LeetSync

---
+++ 3558-find-a-safe-walk-through-a-grid/find-a-safe-walk-through-a-grid.cpp
@@ -0,0 +1,59 @@
+class Solution {
+public:
+    bool findSafeWalk(vector<vector<int>>& grid, int health) {
+
+        int m = grid.size();
+        int n = grid[0].size();
+
+        vector<vector<int>> dist(m, vector<int>(n, INT_MAX));
+
+        priority_queue<
+            pair<int,pair<int,int>>,
+            vector<pair<int,pair<int,int>>>,
+            greater<pair<int,pair<int,int>>> > pq;
+
+        dist[0][0] = grid[0][0];
+
+        pq.push({dist[0][0], {0,0}});
+
+        int dx[4] = {-1,1,0,0};
+        int dy[4] = {0,0,-1,1};
+
+        while(!pq.empty()){
+
+            auto curr = pq.top();
+            pq.pop();
+
+            int cost = curr.first;
+            int x = curr.second.first;
+            int y = curr.second.second;
+
+            if(x == m-1 && y == n-1){
+                return cost < health;
+            }
+
+            if(cost > dist[x][y])
+                continue;
+
+            for(int k=0;k<4;k++){
+
+                int nx = x + dx[k];
+                int ny = y + dy[k];
+
+                if(nx<0 || ny<0 || nx>=m || ny>=n)
+                    continue;
+
+                int newCost = cost + grid[nx][ny];
+
+                if(newCost < dist[nx][ny]){
+
+                    dist[nx][ny] = newCost;
+
+                    pq.push({newCost,{nx,ny}});
+                }
+            }
+        }
+
+        return false;
+    }
+};
