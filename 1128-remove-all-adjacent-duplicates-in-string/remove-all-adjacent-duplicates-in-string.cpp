91e37311ea7e09afae8a5795308768f3895eab7b
Ayush20005 <ayushjaiswa.apsr405@gmail.com>
2026-08-03 19:17:22 +0530

    Time: 9 ms (33.11%) | Memory: 14.2 MB (58.72%) - LeetSync

--- 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.cpp
+++ 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     string removeDuplicates(string s) {
+      /*
        string stack;
 
     for (char c : s) {
@@ -12,5 +13,24 @@ public:
     }
 
     return stack;
+    }
+    */
+    stack<char>st;
+    for(char c:s){
+        if(!st.empty() && st.top()==c){
+            st.pop();
+        }
+        else{
+            st.push(c);
+        }
+    }
+    string ans="";
+    while(!st.empty()){
+        ans+=st.top();
+        st.pop();
+          
+    }
+    reverse(ans.begin(),ans.end());
+  return ans;
     }
 };
