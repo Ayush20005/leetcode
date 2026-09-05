3cb7731e2f5a06f9848f2c0bb78a5307b681658c
Ayush20005 <ayushjaiswa.apsr405@gmail.com>
2026-09-06 01:59:41 +0530

    Time: 3 ms (67.25%) | Memory: 14.9 MB (44.09%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -5,12 +5,23 @@ public:
       
       unordered_map<int,int>mp;
       int n=nums.size();
+      // arr[]=[2,7,11,15]
       for( int i=0;i<n;i++){
         int complement= target- nums[i];
+        //when i=0.
+        //complement=9-2=7
+
         if(mp.find(complement)!=mp.end())
+        //but the map is empty = mp{}.
+
         {return{mp[complement],i};
+        // return true for : {mp(2),1} when i=1; and complement=9-7=2.
         }
+
+
         mp[nums[i]]=i; // this will execute if the complement value is not found in the map.
+        //{ 2:0}will be store in the map.
+        
       }
       
        
