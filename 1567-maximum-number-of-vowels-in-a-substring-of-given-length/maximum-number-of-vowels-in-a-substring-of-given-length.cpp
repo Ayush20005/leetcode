9836496b86b91d508d5ffd4b9928b8179a1997c0
Ayush20005 <ayushjaiswa.apsr405@gmail.com>
2026-09-13 00:20:07 +0530

    Time: 0 ms (100.00%) | Memory: 13.1 MB (89.61%) - LeetSync

--- 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.cpp
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     int maxVowels(string s, int k) {
-
-        int n = s.size();
+        int maxi=0;
+        int left=0;
         int count = 0;
 
+/*
         // Count vowels in the first window
         for (int i = 0; i < k; i++) {
-            if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' ||
-                s[i] == 'o' || s[i] == 'u') {
+            if (s[i]) {
                 count++;
             }
         }
@@ -17,22 +17,49 @@ public:
 
         // Slide the window
         for (int i = k; i < n; i++) {
-
             // Add new character
-            if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' ||
-                s[i] == 'o' || s[i] == 'u') {
+            if (isVowel(s[i])) {
                 count++;
             }
 
             // Remove left character
-            if (s[i - k] == 'a' || s[i - k] == 'e' || s[i - k] == 'i' ||
-                s[i - k] == 'o' || s[i - k] == 'u') {
-                count--;
-            }
+            if (isVowel(s[i - k])) {
+                count--;  // Only decrease if it was a vowel
+            } 
+            // If it was not a vowel, we do nothing in else.
 
+            // Update the maximum
             maxVowel = max(maxVowel, count);
         }
 
         return maxVowel;
+
+        */
+for(int i=0;i<s.size();i++){
+
+         if(s[i]=='A'||s[i]=='E'||s[i]=='I'||s[i]=='O'||s[i]=='U'||s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u'){
+        count++;
+    }
+
+
+    if(i-left+1==k){
+
+
+maxi=max(maxi,count);
+      if (s[left] == 'A' || s[left] == 'E' ||
+                    s[left] == 'I' || s[left] == 'O' ||
+                    s[left] == 'U' ||
+                    s[left] == 'a' || s[left] == 'e' ||
+                    s[left] == 'i' || s[left] == 'o' ||
+                    s[left] == 'u') {
+                    count--;
+                }
+    
+   left++; 
+    }
+
+
+}
+return maxi;
     }
 };
